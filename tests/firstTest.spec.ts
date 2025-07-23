82984a4696b0dea92bebf357a92d88e1cc99d095
Sameh <sameh.abdelhaleem95@gmail.com>
2025-07-24 01:14:42 +0300

    use different locators to get UI components

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,5 +1,67 @@
 import { test } from "@playwright/test";
 
-test("first test", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
+  await page.getByText("Forms").click();
+  await page.getByText("Form Layouts").click();
+});
+
+test("Locator syntax rules", async ({ page }) => {
+  // by tag name
+  await page.locator("input").first().click();
+
+  // by ID
+  await page.locator("#inputEmail1").click();
+
+  // by class name
+  page.locator(".shape-rectangle");
+
+  // by attribute
+  page.locator("[placeholder='Email']");
+  // by entire class name
+  page.locator(
+    "[class='input-full-width size-medium status-basic shape-rectangle nb-transition']"
+  );
+
+  //combined locators
+  page.locator("input[placeholder='Email']");
+  // by xpath NOT RECOMEMENDED
+  page.locator("//*[@id='inputEmail1']");
+  // by partial text
+  page.locator(":text('Using')");
+
+  // by exact text
+  page.locator(":text-is('Using the Grid')");
+});
+
+test("User facing locators", async ({ page }) => {
+  await page.getByRole("textbox", { name: "Email" }).first().click();
+  await page.getByRole("button", { name: "Sign in" }).first().click();
+  await page.getByLabel("Email").first().click();
+  await page.getByPlaceholder("Jane Doe").click();
+  await page.getByText("Using the Grid").click();
+  await page.getByTestId("SignIn").click();
+});
+
+test("locating child elements", async ({ page }) => {
+  await page.locator("nb-card nb-radio :text-is('Option 1')").click();
+  await page
+    .locator("nb-card")
+    .getByRole("button", { name: "Sign in" })
+    .first()
+    .click();
+});
+test("locating Parent elements", async ({ page }) => {
+  await page
+    .locator("nb-card", { hasText: "Using the Grid" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+  await page.locator("nb-card", { has: page.locator("#inputEmail1") }).click();
+  await page.locator("nb-card").filter({ hasText: "Basic form" }).click();
+  await page
+    .locator("nb-card")
+    .filter({ has: page.locator("nb-checkbox") })
+    .filter({ hasText: "Sign in" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
 });
